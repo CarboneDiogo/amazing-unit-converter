adfb515049f895196f82baaf471b35c4a665096a
Diogo Carbone <diogo.carbone@gmail.com>
2024-02-04 21:46:20 -0700

    Added tittle and changed description

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,11 +3,11 @@ import NavItem from "./NavItem";
 
 const Navbar = (props) => {
   const activeTab = props.activeTab;
-  
+
   return (
     <nav className="flex flex-row justify-between ">
 <div className="hover:cursor-pointer hover:text-black hover:bg-white duration-500 p-2">
-  <p>Home</p>
+  <p>The Awesome Unit Converter</p>
 </div>
 <div className="flex flex-row space-x-6">
     <NavItem navItem="Weight" />
