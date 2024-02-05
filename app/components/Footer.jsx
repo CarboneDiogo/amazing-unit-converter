59dbb769f8def0a06ad0f9dee8e3ea7d5c6a5792
Diogo Carbone <diogo.carbone@gmail.com>
2024-02-04 18:19:45 -0700

    removed single quotes from footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex flex-row justify-end'>
+    <div className="flex flex-row justify-end">
         <p>
         Created by: 
         </p>
-        <p className='text-purple-500 pl-2'>Bruxo Troop</p>
+        <p className="text-purple-500 pl-2">Nassubi Troop</p>
     </div>
   )
 }
